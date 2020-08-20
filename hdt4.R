53278f4092bbad8d47d32a5676a8163c6efd4ff2
RodrigoZea <zearodrigo37@gmail.com>
2020-08-19 21:03:42 -0600

    Commit inicial

---
+++ hdt4.R
@@ -0,0 +1,21 @@
+# Instalación de paquetes necesarios para procesar datos:
+install.packages("tm")
+install.packages("wordcloud")
+install.packages("quanteda")
+install.packages("tidyr")
+
+# Importación de paquetes
+library(tm)
+library(wordcloud)
+library(quanteda)
+library(tidyr)
+library(ggplot2)
+
+# Limpieza y preprocesamiento de datos.
+#   - Normalizar Texto -> minuscula/mayuscula
+#   - Remover  caracteres especiales
+#   - Remover  URLs
+#   - Remover emoticones
+#   - Remover signos de puntuacion
+#   - Remover articulos, preposiciones y conjunciones
+#   - Remover numeros si no aportan nada.
